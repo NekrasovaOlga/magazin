8ac0709711d568ecde0bed0849f0cf4ca782da3f
LisenkoKalina <nekrasova.kalina@yandex.ru>
2020-05-29 23:05:29 +0300

    Добавлен блок dicsover и слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $('.reviews__box').slick({
     arrows: false,
 });
+
+$('.slider').slick({
+    arrows: false,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true
+});
